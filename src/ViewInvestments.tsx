5a29df26e7a4c198031f02f45f4d4fba15da5a07
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-06-12 10:22:57 +0200

    Successfully retrieving data from the api.

--- src/ViewInvestments.tsx
+++ src/ViewInvestments.tsx
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-
+import axios from "axios";
 export class ViewInvestments extends Component<any, any>{
     constructor(props:any) {
         super(props);
@@ -7,11 +7,21 @@ export class ViewInvestments extends Component<any, any>{
             investment: {}
         }
     }
-render() {
+   async componentDidMount() {
+        axios.get('https://localhost:5001/api/Investment/1').then(response=>{
+            console.log(response.data);
+            this.setState((investment:object)=>({
+                investment:response.data
+            }))
+        })
+
+    }
+
+    render() {
     return (
         <div>
-            <p>Username:</p>
-            <p>Amount:</p>
+            <p>Username:  {this.state.investment.username}</p>
+            <p>Amount:  {this.state.investment.amount}</p>
             <p>Interest:</p>
         </div>
     );
