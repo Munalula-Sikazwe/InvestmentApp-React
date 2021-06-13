7f9c4f970204a5a30969991b50ffaf2138d07762
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-06-13 08:14:31 +0200

    Changed navbar to links.

--- src/Components/NavbarComponent/NavbarComponent.tsx
+++ src/Components/NavbarComponent/NavbarComponent.tsx
@@ -11,29 +11,29 @@ render() {
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <a className="navbar-brand" href="#">Investment App</a>
+                 <Link className='navbar-brand' to='/'>Investment App</Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
                         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
-                        <Link to='/'>
-                            <li className="nav-item active">
-                                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-                            </li>
-                        </Link>
 
-                        <Link to='/make-investment'>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Make Investment</a>
+                            <li className="nav-item active">
+                                <Link className='nav-link' to='/'>Home <span className="sr-only">(current)</span></Link>
                             </li>
-                        </Link>
-                      <Link to='/view-investments'>
+
+
+
+                            <li className="nav-item">
+                               <Link className='nav-link' to='/make-investment'>Make Investment</Link>
+                            </li>
+
+
                           <li className="nav-item">
-                              <a className="nav-link" href="#">View Investments</a>
+                              <Link className='nav-link' to='/view-investments'>View Investments</Link>
                           </li>
-                      </Link>
+
 
 
                     </ul>
