c4b7dae032282808a4cc2708a843eae01448dbb2
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-06-13 17:08:16 +0200

    changed  react-router-dom from dev dependencies

--- src/Components/MakeInvestmentComponent/InvestmentComponent.tsx
+++ src/Components/MakeInvestmentComponent/InvestmentComponent.tsx
@@ -1,7 +1,6 @@
 import React,{ Component } from "react";
 import axios from "axios";
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 interface InvestmentState {
     Username: string;
     Amount: number;
