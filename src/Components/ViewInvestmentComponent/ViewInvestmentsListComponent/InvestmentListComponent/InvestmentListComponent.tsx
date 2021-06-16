5a28609c5b1ab9b346aaad2a307760591fb34f7c
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-06-16 20:11:53 +0200

    Created investment List Component to hold investment lists.

---
+++ src/Components/ViewInvestmentComponent/ViewInvestmentsListComponent/InvestmentListComponent/InvestmentListComponent.tsx
@@ -0,0 +1,9 @@
+import React,{FC} from "react";
+import {Investmentstate} from "../../ViewInvestmentsComponent";
+const InvestmentListComponent:FC<Investmentstate> = (props)=>{
+    return(
+        <div>
+
+        </div>
+    )
+}
